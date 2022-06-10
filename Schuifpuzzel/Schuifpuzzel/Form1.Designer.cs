df98433c27d99107299d0221b913ed31f00ef9d3
Hakulax28 <83372318+Hakulax28@users.noreply.github.com>
2022-06-10 08:23:28 +0200

    Add files via upload

---
+++ Schuifpuzzel/Schuifpuzzel/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Schuifpuzzel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnBegin = new System.Windows.Forms.Button();
+            this.TxtAantal = new System.Windows.Forms.TextBox();
+            this.ResetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(469, 24);
+            this.menuStrip1.TabIndex = 15;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.ResetToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.NewGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.HelpToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 373);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Aantal Moves";
+            // 
+            // BtnBegin
+            // 
+            this.BtnBegin.Location = new System.Drawing.Point(209, 361);
+            this.BtnBegin.Name = "BtnBegin";
+            this.BtnBegin.Size = new System.Drawing.Size(245, 36);
+            this.BtnBegin.TabIndex = 20;
+            this.BtnBegin.Text = "Begin";
+            this.BtnBegin.UseVisualStyleBackColor = true;
+            this.BtnBegin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TxtAantal
+            // 
+            this.TxtAantal.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.TxtAantal.Location = new System.Drawing.Point(92, 370);
+            this.TxtAantal.Name = "TxtAantal";
+            this.TxtAantal.Size = new System.Drawing.Size(100, 20);
+            this.TxtAantal.TabIndex = 21;
+            // 
+            // ResetToolStripMenuItem
+            // 
+            this.ResetToolStripMenuItem.Name = "ResetToolStripMenuItem";
+            this.ResetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ResetToolStripMenuItem.Text = "Reset";
+            this.ResetToolStripMenuItem.Click += new System.EventHandler(this.ResetToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 408);
+            this.Controls.Add(this.TxtAantal);
+            this.Controls.Add(this.BtnBegin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnBegin;
+        private System.Windows.Forms.TextBox TxtAantal;
+        private System.Windows.Forms.ToolStripMenuItem ResetToolStripMenuItem;
+    }
+}
+
---
+++ Schuifpuzzel/Schuifpuzzel/Form1.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Schuifpuzzel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            StartScherm();
+            InitializeComponent();
+        }
+
+        readonly Button[] arrayButtonCijfers = new Button[16];
+        Random r = new Random();
+        List<int> listGetallen = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        
+        int intTeller = 0;
+        
+        public void StartScherm()
+        {
+            string strCijfer = "123456789101112131415";
+            int j = 0;
+            int k = 0;
+            int l = 0;
+            for (int i = 0; i < arrayButtonCijfers.Length; i++)
+            {
+                arrayButtonCijfers[i] = new Button();
+                arrayButtonCijfers[i].BackColor = Color.Purple;
+                arrayButtonCijfers[i].TextAlign = ContentAlignment.MiddleCenter;
+                arrayButtonCijfers[i].Text = (i + 1).ToString();
+                arrayButtonCijfers[i].ForeColor = Color.White;
+                arrayButtonCijfers[i].Size = new Size(75, 75);
+                arrayButtonCijfers[i].Tag = (i);
+                if (i < 4)
+                {
+                    arrayButtonCijfers[i].Location = new Point(25 + i * 75, 45);
+                }
+                else if (i < 8)
+                {
+                    arrayButtonCijfers[i].Location = new Point(25 + j * 75, 120);
+                    j++;
+                }
+                else if (i < 12)
+                {
+                    arrayButtonCijfers[i].Location = new Point(25 + k * 75, 195);
+                    k++;
+                }
+                else
+                {
+                    arrayButtonCijfers[i].Location = new Point(25 + l * 75, 270);
+                    l++;
+                }
+                if (i == 15)
+                {
+                    arrayButtonCijfers[i].Text = "";
+                }
+                arrayButtonCijfers[i].Click += ButtonOnClickArrayButtonLetter;
+                this.Controls.Add(arrayButtonCijfers[i]);
+            }
+        }
+
+        public void SchuifMiddelsteVier(int intIndex, string strHelp)
+        {
+            if (arrayButtonCijfers[intIndex - 1].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex - 1].Text;
+                arrayButtonCijfers[intIndex - 1].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+            if (arrayButtonCijfers[intIndex + 1].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex + 1].Text;
+                arrayButtonCijfers[intIndex + 1].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+            if (arrayButtonCijfers[intIndex - 4].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex - 4].Text;
+                arrayButtonCijfers[intIndex - 4].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+            if (arrayButtonCijfers[intIndex + 4].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex + 4].Text;
+                arrayButtonCijfers[intIndex + 4].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+
+        }
+
+        public void SchuifZijkantRechts(int intIndex, string strHelp)
+        {
+            if (arrayButtonCijfers[intIndex - 4].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex - 4].Text;
+                arrayButtonCijfers[intIndex - 4].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+            if (arrayButtonCijfers[intIndex - 1].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex - 1].Text;
+                arrayButtonCijfers[intIndex - 1].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+            if (arrayButtonCijfers[intIndex + 4].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex + 4].Text;
+                arrayButtonCijfers[intIndex + 4].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+        }
+
+        public void SchuifZijkantLinks(int intIndex, string strHelp)
+        {
+            if (arrayButtonCijfers[intIndex - 4].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex - 4].Text;
+                arrayButtonCijfers[intIndex - 4].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+            if (arrayButtonCijfers[intIndex + 1].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex + 1].Text;
+                arrayButtonCijfers[intIndex + 1].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+            if (arrayButtonCijfers[intIndex + 4].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex + 4].Text;
+                arrayButtonCijfers[intIndex + 4].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+        }
+
+        public void SchuifBovenkant(int intIndex, string strHelp)
+        {
+            if (arrayButtonCijfers[intIndex + 4].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex + 4].Text;
+                arrayButtonCijfers[intIndex + 4].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+            if (arrayButtonCijfers[intIndex - 1].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex - 1].Text;
+                arrayButtonCijfers[intIndex - 1].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+            if (arrayButtonCijfers[intIndex + 1].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex + 1].Text;
+                arrayButtonCijfers[intIndex + 1].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+        }
+        
+        public void SchuifOnderkant(int intIndex, string strHelp)
+        {
+            if (arrayButtonCijfers[intIndex + 1].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex + 1].Text;
+                arrayButtonCijfers[intIndex + 1].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+            if (arrayButtonCijfers[intIndex - 4].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex - 4].Text;
+                arrayButtonCijfers[intIndex - 4].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+            if (arrayButtonCijfers[intIndex - 1].Text == "")
+            {
+                strHelp = arrayButtonCijfers[intIndex - 1].Text;
+                arrayButtonCijfers[intIndex - 1].Text = arrayButtonCijfers[intIndex].Text;
+                arrayButtonCijfers[intIndex].Text = strHelp;
+            }
+        }
+
+        private void ButtonOnClickArrayButtonLetter(object sender, EventArgs eventArgs)
+        {
+            Button button = (Button)sender;
+            //MessageBox.Show(button.Tag.ToString());
+            int intIndex = int.Parse(button.Tag.ToString());
+            string strHelp = "";
+            intTeller++;
+
+            switch (intIndex)
+            {
+                case 0:
+                    if (arrayButtonCijfers[intIndex + 1].Text == "")
+                    {
+                        strHelp = arrayButtonCijfers[intIndex + 1].Text;
+                        arrayButtonCijfers[intIndex + 1].Text = arrayButtonCijfers[intIndex].Text;
+                        arrayButtonCijfers[intIndex].Text = strHelp;
+                    }
+                    if (arrayButtonCijfers[intIndex + 4].Text == "")
+                    {
+                        strHelp = arrayButtonCijfers[intIndex + 4].Text;
+                        arrayButtonCijfers[intIndex + 4].Text = arrayButtonCijfers[intIndex].Text;
+                        arrayButtonCijfers[intIndex].Text = strHelp;
+                    }
+                    break;
+                case 1:
+                    SchuifBovenkant(intIndex, strHelp);
+                    break;
+                case 2:
+                    SchuifBovenkant(intIndex, strHelp);
+                    break;
+                case 3:
+                    if (arrayButtonCijfers[intIndex - 1].Text == "")
+                    {
+                        strHelp = arrayButtonCijfers[intIndex - 1].Text;
+                        arrayButtonCijfers[intIndex - 1].Text = arrayButtonCijfers[intIndex].Text;
+                        arrayButtonCijfers[intIndex].Text = strHelp;
+                    }
+                    if (arrayButtonCijfers[intIndex + 4].Text == "")
+                    {
+                        strHelp = arrayButtonCijfers[intIndex + 4].Text;
+                        arrayButtonCijfers[intIndex + 4].Text = arrayButtonCijfers[intIndex].Text;
+                        arrayButtonCijfers[intIndex].Text = strHelp;
+                    }
+                    break;
+                case 4:
+                    SchuifZijkantLinks(intIndex, strHelp);
+                    break;
+                case 5:
+                    SchuifMiddelsteVier(intIndex, strHelp);
+                    break;
+                case 6:
+                    SchuifMiddelsteVier(intIndex, strHelp);
+                    break;
+                case 7:
+                    SchuifZijkantRechts(intIndex, strHelp);
+                    break;
+                case 8:
+                    SchuifZijkantLinks(intIndex, strHelp);
+                    break;
+                case 9:
+                    SchuifMiddelsteVier(intIndex, strHelp);
+                    break;
+                case 10:
+                    SchuifMiddelsteVier(intIndex, strHelp);
+                    break;
+                case 11:
+                    SchuifZijkantRechts(intIndex, strHelp);
+                    break;
+                case 12:
+                    if (arrayButtonCijfers[intIndex + 1].Text == "")
+                    {
+                        strHelp = arrayButtonCijfers[intIndex + 1].Text;
+                        arrayButtonCijfers[intIndex + 1].Text = arrayButtonCijfers[intIndex].Text;
+                        arrayButtonCijfers[intIndex].Text = strHelp;
+                    }
+                    if (arrayButtonCijfers[intIndex - 4].Text == "")
+                    {
+                        strHelp = arrayButtonCijfers[intIndex - 4].Text;
+                        arrayButtonCijfers[intIndex - 4].Text = arrayButtonCijfers[intIndex].Text;
+                        arrayButtonCijfers[intIndex].Text = strHelp;
+                    }
+                    break;
+                case 13:
+                    SchuifOnderkant(intIndex, strHelp);
+                    break;
+                case 14:
+                    SchuifOnderkant(intIndex, strHelp);
+                    break;
+                case 15:
+                    if (arrayButtonCijfers[intIndex - 1].Text == "")
+                    {
+                        strHelp = arrayButtonCijfers[intIndex - 1].Text;
+                        arrayButtonCijfers[intIndex - 1].Text = arrayButtonCijfers[intIndex].Text;
+                        arrayButtonCijfers[intIndex].Text = strHelp;
+                    }
+                    if (arrayButtonCijfers[intIndex - 4].Text == "")
+                    {
+                        strHelp = arrayButtonCijfers[intIndex - 4].Text;
+                        arrayButtonCijfers[intIndex - 4].Text = arrayButtonCijfers[intIndex].Text;
+                        arrayButtonCijfers[intIndex].Text = strHelp;
+                    }
+                    break;
+            }
+            TxtAantal.Text = intTeller.ToString();
+            CheckPuzzel();
+        }
+
+        public void ShuffleCijfers()
+        {
+            for (int i = 0; i < arrayButtonCijfers.Length; i++)
+            {
+                if (i < 15)
+                {
+                    int intRandomGetal = r.Next(listGetallen.Count);
+                    int intGetal = listGetallen[intRandomGetal];
+                    arrayButtonCijfers[i].Text = intGetal.ToString();
+                    listGetallen.Remove(intGetal);
+                }
+                else
+                {
+                    arrayButtonCijfers[i].Text = "";
+                }
+            }
+        }
+
+        private void NewGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("SchuifPuzzel. Probeer er voor te zorgen dat alles cijfers weer normaal komen te staan.");
+        }
+
+        private void HelpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ShuffleCijfers();
+        }
+
+        private void ResetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            intTeller = 0;
+        }
+
+        private void CheckPuzzel()
+        {
+            int intCheck = 0;
+            for(int i = 0; i < 14; i++)
+            {
+                if(arrayButtonCijfers[i + 1].Tag.ToString() == arrayButtonCijfers[i].Text)
+                {
+                    intCheck++;
+                }
+            }
+            if(intCheck == 14)
+            {
+                MessageBox.Show("U hebt het opgelost!");
+            }
+        }
+    }
+}
