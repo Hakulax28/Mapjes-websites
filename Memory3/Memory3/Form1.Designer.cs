336ca14bdbc09955194ecf44ff7df10fbc865099
Hakulax28 <83372318+Hakulax28@users.noreply.github.com>
2022-06-10 08:20:32 +0200

    Add files via upload

---
+++ Memory3/Memory3/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Memory3
+{
+    partial class GameWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ScoreCount = new System.Windows.Forms.Label();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(825, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 59);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Try Again";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Book", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(846, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 17;
+            // 
+            // ScoreCount
+            // 
+            this.ScoreCount.AutoSize = true;
+            this.ScoreCount.Font = new System.Drawing.Font("Franklin Gothic Book", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreCount.Location = new System.Drawing.Point(889, 19);
+            this.ScoreCount.Name = "ScoreCount";
+            this.ScoreCount.Size = new System.Drawing.Size(22, 24);
+            this.ScoreCount.TabIndex = 16;
+            this.ScoreCount.Text = "0";
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Font = new System.Drawing.Font("Franklin Gothic Book", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.Location = new System.Drawing.Point(821, 19);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(67, 24);
+            this.ScoreLabel.TabIndex = 15;
+            this.ScoreLabel.Text = "Score:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick_1);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 500;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick_1);
+            // 
+            // GameWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 781);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ScoreCount);
+            this.Controls.Add(this.ScoreLabel);
+            this.Name = "GameWindow";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.GameWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ScoreCount;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ Memory3/Memory3/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory3
+{
+    public partial class GameWindow : Form
+    {
+        //int score = 0;
+        Random location = new Random();
+        //List<int> X = new List<int>();
+        //List<int> Y = new List<int>();
+        List<Point> points = new List<Point>();
+
+        int intPicNumber;
+        string strImage1, strImage2;
+
+        PictureBox PendingImage1;
+        PictureBox PendingImage2;
+
+        // een array maken die bestaat uit pictureboxen
+        PictureBox[] arrayPictureBoxKaart = new PictureBox[48];
+
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+
+        readonly Random random = new Random();
+        private void GameWindow_Load(object sender, EventArgs e)
+        {
+            intPicNumber = random.Next(1, 24);
+            
+            int intRij = 0, intKolom = 15;
+            int intTeller = 0;
+            for (int i = 0; i < arrayPictureBoxKaart.Length; i++)
+            {
+                arrayPictureBoxKaart[i] = new PictureBox();
+
+                arrayPictureBoxKaart[i].Size = new Size(80, 120);
+                arrayPictureBoxKaart[i].SizeMode = PictureBoxSizeMode.Zoom;
+                arrayPictureBoxKaart[i].Tag = i % 24;
+                arrayPictureBoxKaart[i].Load("Images/" + (i % 24).ToString() + ".png");
+                arrayPictureBoxKaart[i].Click += OnClick;
+                if (intTeller < 8)
+                {
+                    arrayPictureBoxKaart[i].Location = new Point(100 + intRij * 80, intKolom);
+                    intRij++;
+                }
+                else
+                {
+                    intTeller = 0;
+                    intRij = 0;
+                    intKolom += 125;
+                    arrayPictureBoxKaart[i].Location = new Point(100 + intRij * 80, intKolom);
+                    intRij++;
+                }
+                this.Controls.Add(arrayPictureBoxKaart[i]);
+                intTeller++;
+            }
+            
+            label1.Text = "5";
+            ScoreCount.Text = Convert.ToString(0);
+            try
+            {
+                foreach (PictureBox picture in arrayPictureBoxKaart)
+                {
+                    picture.Enabled = false;
+                    points.Add(picture.Location);
+                }
+            }
+            catch { };
+
+            try
+            {
+                foreach (PictureBox picture in arrayPictureBoxKaart)
+                {
+                    int next = location.Next(points.Count);
+                    Point p = points[next];
+                    picture.Location = p;
+                    points.Remove(p);
+                }
+            }
+            catch { };
+            
+            timer2.Start();
+            timer1.Start();
+        }
+        //bijna perfect, soms werkt het niet.
+        private void OnClick(object sender, EventArgs eventArgs)
+        {
+            PictureBox pictureBox = (PictureBox)sender;
+            pictureBox.Load("Images/" + (pictureBox.Tag).ToString() + ".png");
+            if (PendingImage1 == null)
+            {
+                PendingImage1 = pictureBox;
+                strImage1 = (PendingImage1.Tag).ToString();
+            }
+            else if (PendingImage1 != null && PendingImage2 == null)
+            {
+                PendingImage2 = pictureBox;
+                strImage2 = (PendingImage2.Tag).ToString();
+            }
+            if (PendingImage1 != null && PendingImage2 != null)
+            {
+                if (strImage1 == strImage2)
+                {
+                    PendingImage1 = null;
+                    PendingImage2 = null;
+                    pictureBox.Enabled = false;
+                    arrayPictureBoxKaart[int.Parse(strImage1) - 1].Enabled = false;
+                    arrayPictureBoxKaart[int.Parse(strImage2) - 1 + 24].Enabled = false;
+                    ScoreCount.Text = Convert.ToString(Convert.ToInt32(ScoreCount.Text) + 10);
+                }
+                else
+                {
+                    ScoreCount.Text = Convert.ToString(Convert.ToInt32(ScoreCount.Text) - 10);
+                    timer3.Start();
+                }
+            }
+        }
+
+        private void timer2_Tick_1(object sender, EventArgs e)
+        {
+            int timer = Convert.ToInt32(label1.Text);
+            timer -= 1;
+            label1.Text = Convert.ToString(timer);
+            if (timer == 0)
+            {
+                timer2.Stop();
+            }
+        }
+        //bijna perfect.
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            try
+            {
+                foreach (PictureBox picture in arrayPictureBoxKaart)
+                {
+                    picture.Enabled = true;
+                    picture.Cursor = Cursors.Hand;
+                    picture.Load("Images/" + (25).ToString() + ".png");
+                }
+            }
+            catch { };
+        }
+
+        private void timer3_Tick_1(object sender, EventArgs e)
+        {
+            timer3.Stop();
+            PendingImage1.Load("Images/" + (25).ToString() + ".png");
+            PendingImage2.Load("Images/" + (25).ToString() + ".png");
+            PendingImage1 = null;
+            PendingImage2 = null;
+        }
+        //De button moet resetten, maar hij werkt niet echt.
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Application.Restart();
+            GameWindow_Load(sender, e);
+        }
+    }
+}
